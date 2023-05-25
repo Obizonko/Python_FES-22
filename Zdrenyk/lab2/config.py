a0a3bf6d446cf1cb3bb21ecfe14573154c60d173
vitalikzdrenyk <129003736+vitalikzdrenyk@users.noreply.github.com>
2023-05-25 22:10:21 +0300

    Add files via upload

---
+++ Zdrenyk/lab2/config.py
@@ -0,0 +1,8 @@
+class Config:   
+    FIELD_HEIGHT = 20   
+    FIELD_WIDTH = 20   
+    COLONY_SIZE = 5    
+    DIMENSION = 2      
+    EMPLOYED_AGENTS = 3
+    SOURCES = 10
+    MAX_FIELD_CAPACITY = 100
